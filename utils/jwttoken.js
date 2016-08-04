e1e2ee0fbebd66a2cb2039e90132ca213c0d29bf
sbaru <sbaru@lantronix.com>
2016-08-04 09:00:54 +0530

    adding json tokens

---
+++ utils/jwttoken.js
@@ -0,0 +1,54 @@
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+const Promise = require('bluebird');
+const pub = fs.readFileSync(path.join(__dirname, '../', 'pub.pem'));
+const priv = fs.readFileSync(path.join(__dirname, '../', 'priv.pem'));
+
+/**
+ * payload should be object
+ */
+
+function signToken(payload) {
+    return new Promise(function(resolve, reject) {
+        jwt.sign(payload, priv, {
+            issuer: 'aimsquant',
+            jwtid: 'jwtid',
+            algorithm: 'RS256',
+            expiresIn: '30d'
+        },
+        function(err, token) {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(token);
+        });
+    });
+}
+
+function verifyToken(token) {
+    return new Promise(function(resolve, reject) {
+        jwt.verify(token, pub, function(err, decoded) {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(decoded);
+        });
+    });
+}
+
+// signToken({
+//     uuid: 'fasdfasd-6c8a-4ce6-b7fd-ebaa7019fc57',
+//     role: 1,
+//     requests: 1,
+//     ttu: 10,
+//     ttl: 60
+// }).then(function(info) {
+//     console.log(info);
+//     verifyToken(info).then(function(details) {
+//         console.log(details);
+//     });
+// })
+
+exports.signToken = signToken;
+exports.verifyToken = verifyToken;
