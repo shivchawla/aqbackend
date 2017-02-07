8b1f64974c83b5023a5079cae24181f508808450
Shiv Chawla <shivchawla2001@gmail.com>
2017-02-07 21:29:40 +0530

    Bug fix: Read mongo settings

--- models/index.js
+++ models/index.js
@@ -6,14 +6,11 @@ Promise.promisifyAll(mongoose);
 var opt = {
     user: config.get('mongo_user'),
     pass: config.get('mongo_pass'),
-    auth: {
-        authdb: 'admin'
-    }
 };
 
 mongoose.connect(config.get('mongo_path'), config.get('mongo_db'), config.get('mongo_port'), opt);
 
-mongoose.set('debug', config.get("mongo_debug"));
+mongoose.set('debug', config.get('mongo_debug'));
 // var db = mongoose.connection;
 // db.on('error', console.error.bind(console, 'connection error:'));
 // db.once('open', function() {
