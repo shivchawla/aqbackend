ae175375c6f2af41f0f7cbe35edf5c06b5fba926
Saurav Biswas <saru.sreyo@gmail.com>
2018-05-30 19:23:02 +0530

    Added Config Service

--- controllers/Marketplace/ConfigService.js
+++ controllers/Marketplace/ConfigService.js
@@ -5,7 +5,8 @@ const Goals = require('../../constants');
 module.exports.getConfig = function(args, res, next) {
     const advice = _.get(args, 'advice.value', 0);
     let response = {};
-    if (advice === 1) {
+    console.log('advice', advice);
+    if (advice === '1') {
         response = Goals;
     }
     res.status(200).send(response);
