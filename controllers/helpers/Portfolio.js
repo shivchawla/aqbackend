262c20671728a3f34dc24f7e88664964fe8a245a
Shiv Chawla <shivchawla2001@gmail.com>
2018-06-13 16:26:07 +0530

    Added unrealized pal (and %) while populating portfolio stats Removed unrealized PnL while converting Julia portfolio to Node portfolio

--- controllers/helpers/Portfolio.js
+++ controllers/helpers/Portfolio.js
@@ -2,7 +2,7 @@
 * @Author: Shiv Chawla
 * @Date:   2018-03-02 11:39:25
 * @Last Modified by:   Shiv Chawla
-* @Last Modified time: 2018-06-12 16:20:54
+* @Last Modified time: 2018-06-13 16:24:48
 */
 'use strict';
 const AdviceModel = require('../../models/Marketplace/Advice');
@@ -128,6 +128,10 @@ function _filterPortfolioForAdvice(portfolio, adviceId) {
 	return {positions: advicePositions};
 }
 
+
+/*
+* Populate pnl stats, netvalue, unrealized Pnl for the portfolio (and individual positions)
+*/
 function _populateStats(portfolio, isAdvice) {
 	return new Promise(resolve => {
 		var port = Object.assign({}, portfolio);
@@ -143,6 +147,9 @@ function _populateStats(portfolio, isAdvice) {
 		positions.map(item => {
 			var weight = totalVal > 0.0 ? (item.quantity*item.lastPrice)/totalVal : 0.0;
 			item.weightInPortfolio = weight;
+			//Added unrealized PnL (and %).
+			item.unrealizedPnl = item.avgPrice > 0 ? (item.lastPrice - item.avgPrice) : 0.0;
+			item.unrealizedPnlPct = item.avgPrice > 0 ? (item.lastPrice - item.avgPrice)/item.avgPrice : 0.0;
 			return item;
 		});
 
