8be0b79d874038219ad571cca4a0851a6670b30f
Shiv Chawla <shivchawla2001@gmail.com>
2018-06-15 16:40:50 +0530

    Experimental Improvement to Forward test controller 1. Forward test were launched frequently on the same machine without waiting for the response from julia server 2. Replaced  sync "ForEach" loop while launching forward test by async Promise.mapSeries

--- controllers/Realtime/ftControl.js
+++ controllers/Realtime/ftControl.js
@@ -8,6 +8,9 @@ const schedule = require('node-schedule');
 const ForwardTestModel = require('../../models/Research/forwardtest');
 const SettingsParser = require('./btSettings.js');
 const serverPort = require('../../index').serverPort;
+const Promise = require('bluebird');
+const _ = require('lodash');
+const APIError = require('../../utils/error');
 
 var numAttempts = {};
 var numRequests = 0;
@@ -76,9 +79,10 @@ function runAllForwardTest() {
             let forwardQueue = Object.keys(data);
 
             // ft will be an array consisting of all active forward tests
-            forwardQueue.forEach(forwardtestId => {
+            //This launches all forward test simultaneously
+            Promise.mapSeries(forwardQueue, function(forwardtestId) {
                 numAttempts[forwardtestId] = 0;
-                handleExecForwardTest(forwardtestId);
+                return handleExecForwardTest(forwardtestId);
             });
         });
     })
@@ -102,8 +106,9 @@ function handleExecForwardTest(forwardtestId) {
             // Update data for successful forward test run
             updateForwardTestResult(forwardtestId, updateData);
 
-            //Delete the forward test from the queue
-            redisUtils.deleteFromRedis(`forwardtest-request-queue-${serverPort}`, forwardtestId, function(err, reply) {
+            //After completion (and updating the data),
+            //Delete the forward test from the ongoing queue as well
+            redisUtils.deleteFromRedis(`forwardtest-ongoing-request-queue-${serverPort}`, forwardtestId, function(err, reply) {
                 if (err) {
                     return console.error(err);
                 } 
@@ -115,13 +120,32 @@ function handleExecForwardTest(forwardtestId) {
 // Forward test executer
 // Will start running a forward test on the Julia server
 function execForwardTest(forwardtestId, cb) {
+
     console.log('execForwardTest is called');
     
-    var connection = getConnectionForFt();
-    console.log(`Choosing connection: ${connection}`)
+    //Delete the forward test from the queue
+    return new Promise((resolve, reject) => {
+        console.log("Deleting request from pending queue");
+        redisUtils.deleteFromRedis(`forwardtest-request-queue-${serverPort}`, forwardtestId, function(err, reply) {
+            if (err) {
+                console.error(err);
+                reject(err);
+            }
 
-    ForwardTestModel.fetchForwardTest({
-        _id: forwardtestId, active: true, error: false, deleted: false}, {})
+            resolve(true); 
+        });
+    })
+    .then(s => {
+        console.log("Adding request to ongoing queue");
+        
+        redisUtils.insertIntoRedis(`forwardtest-ongoing-request-queue-${serverPort}`, item._id, 1);
+        
+        var connection = getConnectionForFt();
+        console.log(`Choosing connection: ${connection}`)
+
+        return ForwardTestModel.fetchForwardTest({
+            _id: forwardtestId, active: true, error: false, deleted: false}, {})
+    })
     .then(ft => {
         if(!ft) {
             throw new Error("Invalid Forward Test");
@@ -180,111 +204,155 @@ function execForwardTest(forwardtestId, cb) {
     })
     .then(argArray => {
 
-        let algorithm = '';
-        let forwardData = '';
-        let ftClient = '';
+        return Promise(resolve => {
 
-        try {
-            ftClient = new WebSocket(connection);
-        } catch (err) {
-            console.log(`Server: ${connection} not available`);
-            handleRequestDenial(forwardtestId);
-            return;
-        }
-        
-        var subscriber = redis.createClient(config.get('redis_port'), config.get('redis_host'))
-        
-        var juliaError = false;
-        var internalError = false;
-        var juliaErrorMessage = '';
-
-        ftClient.on('open', function() {
-            console.log('Connection Open');
-
-            //Delete any data for this backtestId in redis
-            //Mosty, it won't be there bu just in case
-            try {
-                console.log(`Deleting existing data for ${forwardtestId}`);
-                redisUtils.deleteKey(`backtest-realtime-${forwardtestId}`);
-                redisUtils.deleteKey(`backtest-final-${forwardtestId}`);
-            } catch(err) {
-                console.log(err);
-                throw new Error("Error Deleting from redis");
-            }
+            let algorithm = '';
+            let forwardData = '';
+            let ftClient = '';
 
             try {
-                ftClient.send(JSON.stringify({args:argArray.join("??##"), requestType:"execute"}));
-            } catch(err) {
-                console.log(err);
-                console.log(`Error: Sending message to WS connection: ${conn}`);
-                handleRequestDenial(forwardtestId, connection);
-                return;
+                ftClient = new WebSocket(connection);
+            } catch (err) {
+                console.log(`Server: ${connection} not available`);
+                APIError.throwJSONError({rerun: 1, message:"Request Denied"});
             }
 
-        });
+            //If connection is successful, return a promise
+            //BASED on output from Julia server 
+            var subscriber = redis.createClient(config.get('redis_port'), config.get('redis_host'))
+            
+            var juliaError = false;
+            var internalError = false;
+            var juliaErrorMessage = '';
 
-        ftClient.on('message', function(data) {
-            //Now only called when connection is refused
-            //Now, real-time data comes through Redis PUB/SUB
-            try {
-                const dataJSON = JSON.parse(data);
+            ftClient.on('open', function() {
+                console.log('Connection Open');
 
-                if(dataJSON.outputtype === "internal") {
-                    if(dataJSON.code == 503) {
-                        handleRequestDenial(forwardtestId, connection);
-                        return;
-                    } else if(dataJSON.code == 200) {
-                        //Request Accepted
-                        subscriber.subscribe(`backtest-final-${forwardtestId}`);
-                        subscriber.subscribe(`backtest-realtime-${forwardtestId}`);
-                    }
-                }
-            } catch (e) {
-                console.log(e);
-                throw new Error("Error parsing Julia server output");
-            }
-        });
-
-        subscriber.on("message", function(channel, message) {
-            if(channel.indexOf("backtest-final") != -1) {
-                var incomingForwardtestId = channel.split("-")[2];
-                 
-                if(message == "backtest-final-output-ready") {
-                    setTimeout(function(){saveData(forwardtestId, cb, juliaError);}, 1000);
-                } 
-            }
-
-            if(channel.indexOf("backtest-realtime") != -1) {
+                //Delete any data for this backtestId in redis
+                //Mosty, it won't be there but just in case
                 try {
-                    const dataJSON = JSON.parse(message);
-               
-                    if(dataJSON.outputtype === 'log' && dataJSON.messagetype === "ERROR") {
-                        juliaError = true;
-                    } else if(dataJSON.outputtype === "internal") {
-                        juliaError = true;
+                    console.log(`Deleting existing data for ${forwardtestId}`);
+                    redisUtils.deleteKey(`backtest-realtime-${forwardtestId}`);
+                    redisUtils.deleteKey(`backtest-final-${forwardtestId}`);
+                } catch(err) {
+                    console.log(err);
+                    APIError.throwJSONError({rerun: 1, message: "Error Deleting from redis"});
+                }
+
+                try {
+                    ftClient.send(JSON.stringify({args:argArray.join("??##"), requestType:"execute"}));
+                } catch(err) {
+                    console.log(err);
+                    console.log(`Error: Sending message to WS connection: ${conn}`);
+                    APIError.throwJSONError({rerun: 1, message: `Error: Sending message to WS connection: ${conn}`});
+                }
+            });
+
+            ftClient.on('message', function(data) {
+                //Now only called when connection is refused
+                //Now, real-time data comes through Redis PUB/SUB
+                try {
+                    const dataJSON = JSON.parse(data);
+
+                    if(dataJSON.outputtype === "internal") {
+                        if(dataJSON.code == 503) {
+                            APIError.throwJSONError({rerun: 1, message:"Available server is not ready to accept request: Error 503"});
+                        } else if(dataJSON.code == 200) {
+                            //Request Accepted
+                            subscriber.subscribe(`backtest-final-${forwardtestId}`);
+                            subscriber.subscribe(`backtest-realtime-${forwardtestId}`);
+
+                            //ONLY condition when it resolves and goes back
+                            resolve(true);
+                        }
+                        
+                    }
+                } catch (e) {
+                    console.log(e);
+                    APIError.throwJSONError({rerun: 0, message: "Error parsing Julia server output"});
+                }
+            });
+
+            subscriber.on("message", function(channel, message) {
+                if(channel.indexOf("backtest-final") != -1) {
+                    var incomingForwardtestId = channel.split("-")[2];
+                     
+                    if(message == "backtest-final-output-ready") {
+                        setTimeout(function(){saveData(forwardtestId, cb, juliaError);}, 1000);
+                    } 
+                }
+
+                //REALTIME output has no signficance in Forward test
+                if(channel.indexOf("backtest-realtime") != -1) {
+                    try {
+                        const dataJSON = JSON.parse(message);
+
+                        if(dataJSON.outputtype === 'log' && dataJSON.messagetype === "ERROR") {
+                            juliaError = true;
+                        } else if(dataJSON.outputtype === "internal") {
+                            juliaError = true;
+                        }
+                    }
+                    catch (e) {
+                        console.log(e);
+                        APIError.throwJSONError({rerun: 0, message: `Error Parsing realtime data for ${forwardtestId}`});
                     }
                 }
-                catch (e) {
-                    console.log(e);
-                    throw new Error(`Error Parsing realtime data for ${forwardtestId}`);
-                }
-            }
-        });
+            });
+        })
     })
     .catch(err => {
-        //console.log(err);
-        cb(err, {updateMessage:"Test could not be completed"});
+        if (err.rerun) {
+            //If error is due to server failure or server unavailability, rerun the request
+            //Else launch another request 
+            handleRequestDenial(forwardtestId);
+        } else {
+            cb(err, {updateMessage:"Test could not be completed"});
+        }
     });
 }
 
 function handleRequestDenial(forwardtestId, connection) {
+
+    //Re-attempt if all mchines are not exhausted
     if(numAttempts[forwardtestId] % config.get('ftmachines').length != 0) {
+        console.log(`Server: ${connection} is unavailable. Moving to next connection`);
+        handleExecForwardTest(forwardtestId);
+    } else {
+        //If all machines are exhausted,
+        //1. DELETE the request from ongoing requests
+        //2. MOVE the request back to pending requests
+        //3. Re-try running all tests (only pending will re-run)
+        console.log(`DENIED: Remove request for ${forwardtestId} from ongoing queue`);    
+        return new Promise((resolve, reject) => {
+            redisUtils.deleteFromRedis(`forwardtest-ongoing-request-queue-${serverPort}`, forwardtestId, function(err, reply) {
+                if (err) {
+                    console.error(err);
+                    reject(err);
+                }
+
+                resolve(true); 
+            });
+        })
+        .then(s => {
+            console.log(`Adding request for ${forwardtestId} back to pending queue`);
+            redisUtils.insertIntoRedis(`forwardtest-request-queue-${serverPort}`, forwardtestId, 1);
+
+            //Run all pending forward test every 60000
+            _.throttle(runAllForwardTest, 60000);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    /*if(numAttempts[forwardtestId] % config.get('ftmachines').length != 0) {
         console.log(`Server: ${connection} is unavailable. Moving to next connection`);
         handleExecForwardTest(forwardtestId);
     } else {
         console.log("All Servers Unavailable!! Will retry in 1 minute");
         setTimeout(function(){handleExecForwardTest(forwardtestId)}, 60000);
-    }
+    }*/
 }
 
 function saveData(forwardtestId, cb, juliaError) {
