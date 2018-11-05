6002181534db17bffb9927547e41f6f8072e3d3e
Shiv Chawla <shivchawla2001@gmail.com>
2018-11-05 18:02:18 +0530

    Bug Fix: reference error (wrong variable reference while populating top stocks)

--- controllers/Marketplace/DailyContestService.js
+++ controllers/Marketplace/DailyContestService.js
@@ -2,7 +2,7 @@
 * @Author: Shiv Chawla
 * @Date:   2018-09-07 17:57:48
 * @Last Modified by:   Shiv Chawla
-* @Last Modified time: 2018-11-05 16:45:01
+* @Last Modified time: 2018-11-05 18:00:55
 */
 
 'use strict';
@@ -302,7 +302,7 @@ module.exports.getDailyContestTopStocks = (args, res, next) => {
 				})	
 			}),
 			Promise.map(topStocksByUsers, function(topStock) {
-				return SecurityHelper.getStockLatestDetail({ticker: topStock.security})
+				return SecurityHelper.getStockLatestDetail({ticker: topStock.ticker})
 				.then(securityDetail => {
 					delete topStock.ticker;
 					return {...topStock, security: securityDetail};
