8cd2a8d9d75f17bf028316bc72d5f266ca2a305f
Shiv Chawla <shivchawla2001@gmail.com>
2019-02-23 13:58:27 +0530

    Added startup script Update REQUIRE

--- Julia/Deploy/.juliarc.jl
+++
@@ -1,9 +0,0 @@
-user = ENV["USER"]
-push!(LOAD_PATH, "/home/$user/raftaar/Engine/")
-push!(LOAD_PATH,"/home/$user/yojak/src/")
-push!(LOAD_PATH,"/home/$user/raftaar/Output/")
-push!(LOAD_PATH,"/home/$user/raftaar/API/")
-push!(LOAD_PATH,"/home/$user/raftaar/Optimizer/")
-
-Pkg.init()
-Pkg.resolve()
