102fc86fc64c1da130cf7344cfcde88ec4d04f5a
shivchawla <shivchawla2001@gmail.com>
2023-11-05 13:54:26 -0500

    Updated mongodb version and connection

--- models/index.js
+++ models/index.js
@@ -1,12 +1,5 @@
 const config = require('config');
 const mongoose = require('mongoose');
-const Promise = require('bluebird');
-Promise.promisifyAll(mongoose);
-
-var opt = {
-    user: config.get('mongo_user'),
-    pass: config.get('mongo_pass'),
-};
 
 var mongo_host = config.get('mongo_path')
 var mongo_port = config.get('mongo_port')
@@ -14,8 +7,15 @@ var mongo_db = config.get('mongo_db')
 var mongo_user = config.get('mongo_user')
 var mongo_pass = config.get('mongo_pass')
 
-mongoose.connect(`mongodb://${mongo_user}:${mongo_pass}@${mongo_host}:${mongo_port}/${mongo_db}?authSource=admin`);
-//mongoose.connect(config.get('mongo_path'), config.get('mongo_db'), config.get('mongo_port'), opt);
+
+var opt = {
+    ...mongo_user!="" && {user: mongo_user},
+    ...mongo_pass!="" && {pass: mongo_pass},
+};
+
+console.log(opt)
+
+mongoose.connect(`mongodb://${mongo_host}:${mongo_port}/${mongo_db}`, opt);
 
 mongoose.set('debug', config.get('mongo_debug'));
 module.exports = mongoose;
